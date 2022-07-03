3c4c46c810bf08f2df275e02cec92231e981263f
chrishoonS <chrishoon90@daum.net>
2022-07-03 23:43:02 +0900

    [1844]게임맵최단거리_20220701_finish

--- src/main/java/chrishoonS/maester/Solution07.java
+++ src/main/java/chrishoonS/maester/Solution07.java
@@ -6,11 +6,11 @@ import java.util.Queue;
 class Solution07 {
     static int answer, N, M;
     static int[] x = {-1,1,0,0}, y = {0,0,-1,1}; //x,y 좌표 setting
-    static boolean[][] isVisit; //맵 방문 여부 체크용
+    static boolean[][] isVisit;
 
     public int solution(int[][] maps) {
-        N = maps.length;
-        M = maps[0].length;
+        N = maps.length;        //가로축
+        M = maps[0].length;     //세로축
         isVisit = new boolean[N][N];
         
         if(bfs(maps) == true)
@@ -18,18 +18,19 @@ class Solution07 {
         else return -1;
     }
 
-    private static boolean bfs(int[][] maps) {
+    private static boolean bfs(int[][] maps) {  //dfs가 아닌 bfs로 단계별로 탐색
         Queue<int[]> queue = new LinkedList<>();    //queue declare
         queue.offer(new int[] {0,0});
-        isVisit[0][0] = true;
+        isVisit[0][0] = true;       //처음 시작점 true setting
 
         while(!queue.isEmpty()){
             int size = queue.size();
             answer++;
             while(--size >= 0){
-                int[] q = queue.poll();
+                int[] q = queue.poll(); //queue에서 꺼낸 값 저장
 
-                if (q[0] == N-1 && q[1] == M-1) return true;
+                if (q[0] == N-1 && q[1] == M-1)
+                    return true;
 
                 for (int i = 0; i < 4; i++) {
                     int nx = q[0] + x[i];
