c4c17778aaab7bfe472a47359099572c4d72415c
chrishoon <chrishoon90@daum.net>
2022-07-05 13:20:19 +0900

    [12978]영어 끝말잇기_12981_ing

---
+++ src/main/java/chrishoonS/summer_winter_2018/Solution09.java
@@ -0,0 +1,13 @@
+package chrishoonS.summer_winter_2018;
+
+class Solution09 {
+    public int[] solution(int n, String[] words) {
+        int[] answer = new int[2];
+        int personNo = 0;   //answer[0] = 사람 순번
+        int failCnt = 0;    //answer[1] = 끝말잇기 실패한 횟수
+
+
+
+        return answer;
+    }
+}
