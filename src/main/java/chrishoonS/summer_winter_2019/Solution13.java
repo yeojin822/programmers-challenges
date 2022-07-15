1636fe36865502677bd9f5cd1cb75eb61a2d7274
chrishoon <chrishoon90@daum.net>
2022-07-15 17:37:22 +0900

    [62048]멀쩡한 사각형_20220715_finish

--- src/main/java/chrishoonS/summer_winter_2019/Solution13.java
+++ src/main/java/chrishoonS/summer_winter_2019/Solution13.java
@@ -3,6 +3,32 @@ package chrishoonS.summer_winter_2019;
 class Solution13 {
     public long solution(int w, int h) {
         long answer = 1;
+        long width = Long.valueOf(w);
+        long height = Long.valueOf(w);
+
+        answer = width * height - (width + height - getGcd(width, height));
+
         return answer;
+    }
+
+    private long getGcd(long width, long height) {
+
+        long tmp = 0;
+        long big = width;
+        long small = height;
+
+        if (width < height) {
+            big = height;
+            small = width;
+        }
+
+        long temp = 0;
+        while (small > 0) {
+            temp = big % small;//나누기
+
+            big = small;//8, 4
+            small = temp;//4, 0
+        }
+        return (big);
     }
 }
