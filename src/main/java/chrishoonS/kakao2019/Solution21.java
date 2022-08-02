c9e0082b0ee294f88f777ae122c4049ee034d776
chrishoon <chrishoon90@daum.net>
2022-08-02 10:52:37 +0900

    [64065]튜플_20220801_finish2

--- src/main/java/chrishoonS/kakao2019/Solution21.java
+++ src/main/java/chrishoonS/kakao2019/Solution21.java
@@ -1,8 +1,28 @@
 package chrishoonS.kakao2019;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Solution21 {
     public int[] solution(String s) {
-        int[] answer = {};
+
+        Map<String, Integer> map = new HashMap<>();
+        Pattern pattern = Pattern.compile("[0-9]+");
+        Matcher matcher = pattern.matcher(s);
+
+        while (matcher.find()) {
+            String n = matcher.group();
+            map.put(n, map.getOrDefault(n, 0) + 1);
+        }
+
+        int size = map.size();
+        int[] answer = new int[size];
+
+        for (String key: map.keySet()) {
+            answer[size - map.get(key)] = Integer.parseInt(key);
+        }
         return answer;
     }
 }
