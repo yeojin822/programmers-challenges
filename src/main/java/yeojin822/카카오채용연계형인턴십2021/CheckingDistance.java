8f044ccbcd6d3d02177131cc3b7a6d49f06eba22
yeojin822 <xlzt2002@gmail.com>
2022-08-05 14:46:26 +0900

    [81302]거리두기 확인하기_20220805 ing

---
+++ src/main/java/yeojin822/카카오채용연계형인턴십2021/CheckingDistance.java
@@ -0,0 +1,12 @@
+package yeojin822.카카오채용연계형인턴십2021;
+
+
+/**
+ * [81302] 거리두기 확인하기
+ */
+public class CheckingDistance {
+    public int[] solution(String[][] places) {
+        int[] answer = {};
+        return answer;
+    }
+}
