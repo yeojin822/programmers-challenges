437e678025028f874217f6771b01a2cf50fb7c87
yeojin822 <xlzt2002@gmail.com>
2022-08-18 15:26:35 +0900

    [17683][3차]방금그곡_20220818

--- src/test/java/yeojin822/KAKAOBLINDRECRUITMENT2018/SongTest.java
+++ src/test/java/yeojin822/KAKAOBLINDRECRUITMENT2018/SongTest.java
@@ -10,5 +10,7 @@ class SongTest {
     @Test
     void solution() {
         assertThat(song.solution("ABCDEFG",new String[]{"12:00,12:14,HELLO,CDEFGAB", "13:00,13:05,WORLD,ABCDEF"})).isEqualTo("HELLO");
+        assertThat(song.solution("CC#BCC#BCC#BCC#B",new String[]{"03:00,03:30,FOO,CC#B", "04:00,04:08,BAR,CC#BCC#BCC#B"})).isEqualTo("FOO");
+        assertThat(song.solution("ABC",new String[]{"12:00,12:14,HELLO,C#DEFGAB", "13:00,13:05,WORLD,ABCDEF"})).isEqualTo("WORLD");
     }
 }
