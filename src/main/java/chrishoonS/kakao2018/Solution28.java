703281309b44b814fedae9be9b48d186ceb23d7a
chrishoon <chrishoon90@daum.net>
2022-08-18 16:03:47 +0900

    [17683]방금그곡_20220818_ing

---
+++ src/main/java/chrishoonS/kakao2018/Solution28.java
@@ -0,0 +1,10 @@
+package chrishoonS.kakao2018;
+
+import java.util.LinkedList;
+
+public class Solution28 {
+    public String solution(String m, String[] musicinfos) {
+        String answer = "";
+        return answer;
+    }
+}
