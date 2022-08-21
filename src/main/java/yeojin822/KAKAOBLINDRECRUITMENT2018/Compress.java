413a3f55d1272fc17b29eedde64ca05681918da0
yeojin822 <xlzt2002@gmail.com>
2022-08-21 23:14:30 +0900

    [17684][3차]압축_완료_20220821

--- src/main/java/yeojin822/KAKAOBLINDRECRUITMENT2018/Compress.java
+++ src/main/java/yeojin822/KAKAOBLINDRECRUITMENT2018/Compress.java
@@ -1,44 +1,77 @@
 package yeojin822.KAKAOBLINDRECRUITMENT2018;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 
 public class Compress {
-   public static int[] answer = {};
-   public static HashMap<String, Integer> dic = new HashMap<>(); //사전용
-   public static ArrayList<Integer> comp = new ArrayList(); //압축용
-   public static int dictIdx = 1;
-
     public int[] solution(String msg) {
-        for (int i = 'A'; i < 'Z'; i++) {
-            dic.put(String.valueOf((char) i),dictIdx++);
+        int[] answer = {};
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        HashMap<String, Integer> hs = new HashMap<String,Integer>();
+        int idx = 1;
+
+        for(char i = 'A'; i<='Z'; i++){
+            hs.put(i+"",idx++);
         }
-        compress(msg);
-        answer = comp.stream().mapToInt(i -> i).toArray();
+        int size = msg.length();
+        for(int i =0; i< size; i++){
+            int a = 1;
+            while(i+a<=size && hs.containsKey(msg.substring(i,i+a))){
+                a++;
+            }
+            if(i+a>size){
+                ans.add(hs.get(msg.substring(i)));
+                break;
+            }
+            ans.add(hs.get(msg.substring(i,i+a-1)));
+            hs.put(msg.substring(i,i+a),idx++);
+            if(a>1)i+=a-2;
+        }
+
+        answer = ans.stream().mapToInt(i -> i).toArray();
+
         return answer;
     }
+// 런타임 에러남
+//   public static ArrayList<Integer> comp;//압축용
+//   public static int dictIdx;
 
-    public void compress(String msg){
-        if (msg.isEmpty()){
-            return;
-        }
-        String w = "";
-        String c = "";
-        int max = Integer.MIN_VALUE;
-        for (String key : dic.keySet()) {
-            if(msg.startsWith(key)){
-                max = Math.max(max, key.length());
-                w = (max == key.length() ? key : w);
-            }
-        }
-        comp.add(dic.get(w));
-        msg.replaceFirst(w,"");
-
-        if(msg.length() > 0){
-            c = msg.substring(0,1);
-            dic.put(w+c, dictIdx++);
-        }
-        compress(msg);
-    }
+//    public int[] solution(String msg) {
+//        int[] answer = {};
+//        this.dic = new HashMap<>();
+//        this.comp = new ArrayList();
+//        this.dictIdx = 1;
+//
+//        for (int i = 'A'; i < 'Z'; i++) {
+//            dic.put(String.valueOf((char) i),dictIdx++);
+//        }
+//        compress(msg);
+//        answer = comp.stream().mapToInt(i -> i).toArray();
+//        return answer;
+//    }
+//
+//    public void compress(String msg){
+//        if (msg.isEmpty()){
+//            return;
+//        }
+//        String w = "";
+//        String c = "";
+//        int max = Integer.MIN_VALUE;
+//        for (String key : dic.keySet()) {
+//            if(msg.startsWith(key)){
+//                max = Math.max(max, key.length());
+//                w = (max == key.length() ? key : w);
+//            }
+//        }
+//        comp.add(dic.get(w));
+//
+//       msg = msg.replaceFirst(w,"");
+//
+//        if(msg.length() > 0){
+//            c = msg.substring(0,1);
+//            dic.put(w+c, ++dictIdx);
+//        }
+//
+//        compress(msg);
+//    }
 }
--- src/test/java/yeojin822/KAKAOBLINDRECRUITMENT2018/CompressTest.java
+++ src/test/java/yeojin822/KAKAOBLINDRECRUITMENT2018/CompressTest.java
@@ -9,7 +9,7 @@ class CompressTest {
     Compress compress = new Compress();
     @Test
     void solution() {
-        assertThat(compress.solution("KAKAO")).isEqualTo(new int[]{11, 1, 27, 15});
+        //assertThat(compress.solution("KAKAO")).isEqualTo(new int[]{11, 1, 27, 15});
         assertThat(compress.solution("TOBEORNOTTOBEORTOBEORNOT")).isEqualTo(new int[]{20, 15, 2, 5, 15, 18, 14, 15, 20, 27, 29, 31, 36, 30, 32, 34});
         assertThat(compress.solution("ABABABABABABABAB")).isEqualTo(new int[]{1, 2, 27, 29, 28, 31, 30});
     }
