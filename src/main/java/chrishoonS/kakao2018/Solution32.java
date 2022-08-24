63285d3507a87b9c436fed1df94dee79c1a2535c
chrishoon <chrishoon90@daum.net>
2022-08-24 13:10:41 +0900

    [17687]n진수 게임_20220824_finish

--- src/main/java/chrishoonS/kakao2018/Solution32.java
+++ src/main/java/chrishoonS/kakao2018/Solution32.java
@@ -2,6 +2,7 @@ package chrishoonS.kakao2018;
 
 public class Solution32 {
     public String solution(int n, int t, int m, int p) {
+        String answer = "";
         String str = "0";
         int count = 0;
 
@@ -10,10 +11,12 @@ public class Solution32 {
             int num = count++;
 
             while (num != 0) {
-                if (num % n >= 10)
+                if (num % n >= 10) {
                     byNum += String.valueOf((char) (num % n + 55));
-                else
+                }
+                else {
                     byNum += String.valueOf(num % n);
+                }
 
                 num /= n;
             }
@@ -21,7 +24,6 @@ public class Solution32 {
             str += new StringBuffer(byNum).reverse().toString();
         }
 
-        String answer = "";
 
         for (int i = 0; i < t; i++)
             answer += String.valueOf(str.charAt(m * i + p - 1));
