a457b19f20285a6711e9aafd9300d28eba182689
chrishoon <chrishoon90@daum.net>
2022-08-29 17:19:23 +0900

    [42890]후보키_20220829_ing

---
+++ src/main/java/chrishoonS/kakao2019/Solution34.java
@@ -0,0 +1,11 @@
+package chrishoonS.kakao2019;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Solution34 {
+    public int solution(String[][] relation) {
+        int answer = 0;
+        return answer;
+    }
+}
