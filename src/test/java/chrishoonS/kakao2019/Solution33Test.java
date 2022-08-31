c8930a4d45ce7454e0f794841968390eaf3a358f
chrishoon <chrishoon90@daum.net>
2022-08-31 09:47:50 +0900

    [42890]후보키_20220831_finish

--- src/test/java/chrishoonS/kakao2019/Solution33Test.java
+++ src/test/java/chrishoonS/kakao2019/Solution33Test.java
@@ -12,6 +12,5 @@ class Solution33Test {
         Solution33 slt33 = new Solution33();
         assertArrayEquals(slt33.solution(new String[] {"Enter uid1234 Muzi", "Enter uid4567 Prodo","Leave uid1234","Enter uid1234 Prodo","Change uid4567 Ryan"}),
                 new String[] {"Prodo님이 들어왔습니다.", "Ryan님이 들어왔습니다.", "Prodo님이 나갔습니다.", "Prodo님이 들어왔습니다."});
-
     }
 }
