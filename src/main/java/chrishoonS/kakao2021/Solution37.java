e2e3cdb53673fa3f5a3ad66569ebf1d27d9b0ed9
chrishoon <chrishoon90@daum.net>
2022-09-07 09:31:36 +0900

    [72411]메뉴 리뉴얼_20220907_finish

--- src/main/java/chrishoonS/kakao2021/Solution37.java
+++ src/main/java/chrishoonS/kakao2021/Solution37.java
@@ -1,55 +1,86 @@
 package chrishoonS.kakao2021;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
 
 public class Solution37 {
-    List<String> answerList = new ArrayList<>();
-    Map<String, Integer> hashMap = new HashMap<>();
+    static int[] map;
+    static int M;
+    static String cur;
+    static HashMap<String, Integer> hmap;
 
-    public String[] solution(String[] orders, int[] course) {
+    public static void Eval() {
+        String t ="";
+        for (int i=0; i<M; i++)
+            if (map[i] == 1)
+                t = t+cur.charAt(i);
+        if (hmap.containsKey(t) == true)
+            hmap.put(t, hmap.get(t) +1);
+        else
+            hmap.put(t, 1);
+    }
 
-        // 1. 각 Order 정렬
-        for (int i = 0; i < orders.length; i++) {
-            char[] arr = orders[i].toCharArray();
-            Arrays.sort(arr);
-            orders[i] = String.valueOf(arr);
+    public static void Combi(int t, int n) {
+        int i;
+
+        if (t == n) {
+            for (i=0; i<t; i++)
+                map[i] = 1;
+            Eval();
+            return;
+        }
+        if (n == 0) {
+            for (i=0; i<t; i++)
+                map[i] = 0;
+            Eval();
+            return;
+        }
+        map[t-1] = 0;
+        Combi(t-1, n);
+        map[t-1] = 1;
+        Combi(t-1, n-1);
+    }
+
+    static String[] solution (String[] s, int[] a) {
+        hmap = new HashMap<String, Integer> ();
+        for (int i=0; i<s.length; i++) {
+            char[] chars = s[i].toCharArray();
+            Arrays.sort(chars);
+            s[i] = new String(chars);
+
+            M = s[i].length();
+            map = new int[M];
+            cur = s[i];
+            for (int j =0; j<a.length; j++)
+                if (M < a[j]) continue;
+                else Combi(M, a[j]);
         }
 
-        // 2. 각 order를 기준으로 courseLength 만큼의 조합 만들기
-        for (int courseLength : course) {
-            for (String order : orders)
-                combination("", order, courseLength);
+        int[] mm = new int[11];
+        int cnt = 0;
+        for( String key : hmap.keySet() )
+            if( hmap.get(key) > 1) {
+                int ll = key.length();
+                int val = hmap.get(key);
+                if (val > mm[ll]) mm[ll] = val;
+            }
 
-            // 3. 가장 많은 조합 answer에 저장
-            if (!hashMap.isEmpty()) {
-                List<Integer> countList = new ArrayList<>(hashMap.values());
-                int max = Collections.max(countList);
-
-                if (max > 1)
-                    for (String key : hashMap.keySet())
-                        if (hashMap.get(key) == max)
-                            answerList.add(key);
-                hashMap.clear();
+        ArrayList<String> rr = new ArrayList<String> ();
+        for( String key : hmap.keySet() ) {
+            int val = hmap.get(key);
+            for (int i=0; i<a.length; i++) {
+                if (key.length() == a[i] && val == mm[a[i]])
+                    rr.add(key);
             }
         }
 
-        Collections.sort(answerList);
-        String[] answer = new String[answerList.size()];
-        for (int i = 0; i < answer.length; i++)
-            answer[i] = answerList.get(i);
+        String[] r = new String[rr.size()];
+        cnt=0;
+        for (String u: rr)
+            r[cnt++] = u;
+        Arrays.sort(r);
 
-        return answer;
-    }
-
-    public void combination(String order, String others, int count) {
-        // 탈출 조건 : count == 0
-        if (count == 0) {
-            hashMap.put(order, hashMap.getOrDefault(order, 0) + 1);
-            return;
-        }
-
-        // 0부터 length까지 조합
-        for (int i = 0; i < others.length(); i++)
-            combination(order + others.charAt(i), others.substring(i + 1), count - 1);
+        return r;
     }
 }
