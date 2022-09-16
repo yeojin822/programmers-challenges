a17a388319d8d0cf61db80ac283e43f486fa2718
chrishoon <chrishoon90@daum.net>
2022-09-16 09:23:08 +0900

    [92335]k진수에서 소수개수 구하기_20220916_finish

--- src/main/java/chrishoonS/kakao2022/Solution40.java
+++ src/main/java/chrishoonS/kakao2022/Solution40.java
@@ -1,28 +1,33 @@
 package chrishoonS.kakao2022;
 
 public class Solution40 {
-    public boolean isprime(long n){
-        if(n <= 1) return false;
-        else if(n == 2) return true;
+    public boolean isPrime(long n){
+        if(n <= 1)
+            return false;
+        else if(n == 2)
+            return true;
+
         for(int i = 2; i <= Math.sqrt(n); i++)
             if(n % i == 0)
                 return false;
+
         return true;
     }
-    public String to_Knum(int n, int k) {
-        String res = "";
+    public String toKnum(int n, int k) {
+        String rst = "";
         while(n > 0) {
-            res = n % k + res;
+            rst = n % k + rst;
             n /= k;
         }
-        return res;
+        return rst;
     }
     public int solution(int n, int k) {
         int answer = 0, i, j;
-        String s = to_Knum(n,k);
+        String s = toKnum(n,k);
+
         for(i = 0; i < s.length(); i = j) {
             for(j = i + 1; j < s.length() && s.charAt(j) != '0'; j++);
-            if(isprime(Long.parseLong(s.substring(i,j))))
+            if(isPrime(Long.parseLong(s.substring(i,j))))
                 answer++;
         }
         return answer;
